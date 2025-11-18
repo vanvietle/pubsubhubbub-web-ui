e6ff42d700917752b62b29d09bc6fb8823456e05
VanVietLe <vietvan05032002@gmail.com>
2025-11-18 22:07:37 +0700

    add deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTest.ts'],
-    pool: 'forks',
-  },
+  base: '/pubsubhubbub-web-ui',
 });
